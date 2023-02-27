6fe1750693b28b0a6f2a3da33710976084b65991
csupreme19 <csupreme19@gmail.com>
2023-02-27 16:30:50 +0900

    ConversionService 사용해보기

--- src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,10 +1,11 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ConverterTest {
+class ConverterTest {
 
     @Test
     void stringToInteger() {
@@ -22,4 +23,19 @@ public class ConverterTest {
         assertThat(result).isEqualTo("10");
     }
 
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        String result = converter.convert(new IpPort("127.0.0.1", 8080));
+
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
 }
